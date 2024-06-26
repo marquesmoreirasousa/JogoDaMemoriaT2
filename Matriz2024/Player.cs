e01224f8fd0e85aab975f50c626e7d9f48e0a139
marquesmoreirasousa <marques.sousa@ifsp.edu.br>
2024-06-26 10:47:30 -0300

    Matrix Examples

---
+++ Matriz2024/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz2024
+{
+    internal class Player
+    {
+        //Atributos
+        private string _name;
+        private int _score;
+        private TimeSpan _gameTime;
+
+        public Player(string name) { 
+            Name = name;
+            Score = 0;
+            GameTime = TimeSpan.FromSeconds(0);
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { 
+                if(!string.IsNullOrEmpty(value))
+                    _name = value; 
+            }
+        }
+
+        public int Score
+        {
+            get { return _score; }
+            set
+            {
+                if (value >= 0)
+                    _score += value;
+            }
+        }
+
+        public TimeSpan GameTime
+        {
+            get { return _gameTime; }
+            set
+            {
+                _gameTime += value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Name +
+            "\nPontuação: " + Score +
+            "\nTempo de Partida: " + GameTime;
+        }
+    }
+}
--- Matriz2024/Program.cs
+++ Matriz2024/Program.cs
@@ -1,94 +1,179 @@
-﻿int[,] tela = new int[4, 4];
-int[,] jogo = new int[4, 4];
-int acertos = 0;
+﻿using System;
+using System.Runtime.CompilerServices;
 
-Random gerador = new Random();
-
-int lin, col;
-//Vamos gerar os pares de números e adicionar
-//na matriz jogo
-for (int i = 1; i <= 8; i++)
+namespace Matriz2024
 {
-    for (int n = 0; n < 2; n++)
+    internal class Program
     {
-        do
-        {
-            lin = gerador.Next(0, 4);
-            col = gerador.Next(0, 4);
-        } while (jogo[lin, col] != 0);
+        static void PrintMatrix(int[,] screen) {
+            Console.Write("   ");
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.Write(" {0}  ", i);
+            }
+            Console.WriteLine("\n  -----------------");
+            //Impressão
+            for (int j = 0; j < 4; j++)
+            {
+                Console.Write("{0} |", j + 1);
+                for (int k = 0; k < 4; k++)
+                {
+                    Console.Write(" {0} |", screen[j, k]);
+                }
+                Console.WriteLine("\n  -----------------");
 
-        jogo[lin, col] = i;
+            }
+            Console.WriteLine();
+        }
+        static void Main(String[] Args)
+        {
+            int[,] tela = new int[4, 4];
+            int[,] jogo = new int[4, 4];
+            int acertos = 0;
+            Player p1 = new Player("Marques");
+            Player p2 = new Player("Alfredo");
+
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(p2.ToString());
+
+
+            Random gerador = new Random();
+            DateTime inicio = DateTime.Now;
+
+            int lin, col;
+            //Vamos gerar os pares de números e adicionar
+            //na matriz jogo
+            for (int i = 1; i <= 8; i++)
+            {
+                for (int n = 0; n < 2; n++)
+                {
+                    do
+                    {
+                        lin = gerador.Next(0, 4);
+                        col = gerador.Next(0, 4);
+                    } while (jogo[lin, col] != 0);
+
+                    jogo[lin, col] = i;
+                }
+            }
+
+            Console.WriteLine();
+            do
+            {
+                //Imprimir conteúdo da matriz tela
+                Program.PrintMatrix(tela);
+
+                int lin1, col1;
+                do
+                {
+                    do
+                    {
+                        Console.WriteLine("Entre com a linha [1, 4]");
+                        lin1 = int.Parse(Console.ReadLine());
+                    } while (lin1 <= 0 || lin1 >= 5);
+
+                    do
+                    {
+                        Console.WriteLine("Entre com a coluna [1, 4]");
+                        col1 = int.Parse(Console.ReadLine());
+                    } while (col1 <= 0 || col1 >= 5);
+
+                    lin1--; //diminuir, pois o usuário não sabe
+                    col1--; //que existe linha e coluna zero
+                            //Atribuir o valor da matriz jogo na tela
+                    if (tela[lin1, col1] != 0)
+                        Console.WriteLine("ERRO: VOCÊ JÁ ESCOLHEU ESTA POSIÇÃO!");
+                } while (tela[lin1, col1] != 0);
+
+
+                tela[lin1, col1] = jogo[lin1, col1];
+
+                Console.Write("   ");
+                for (int i = 1; i <= 4; i++)
+                {
+                    Console.Write(" {0}  ", i);
+                }
+                Console.WriteLine("\n  -----------------");
+                //Impressão
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write("{0} |", j + 1);
+                    for (int k = 0; k < 4; k++)
+                    {
+                        Console.Write(" {0} |", tela[j, k]);
+                    }
+                    Console.WriteLine("\n  -----------------");
+
+                }
+                Console.WriteLine();
+
+                int lin2, col2;
+                do
+                {
+                    do
+                    {
+                        Console.WriteLine("Entre com a linha [1, 4]");
+                        lin2 = int.Parse(Console.ReadLine());
+                    } while (lin2 <= 0 || lin2 >= 5);
+                    do
+                    {
+                        Console.WriteLine("Entre com a coluna [1, 4]");
+                        col2 = int.Parse(Console.ReadLine());
+                    } while (col2 <= 0 || col2 >= 5);
+
+                    lin2--; //diminuir, pois o usuário não sabe
+                    col2--; //que existe linha e coluna zero
+                            //Atribuir o valor da matriz jogo na tela
+                    if (tela[lin2, col2] != 0)
+                        Console.WriteLine("ERRO: VOCÊ JÁ ESCOLHEU ESTA POSIÇÃO!");
+                } while (tela[lin2, col2] != 0);
+
+                tela[lin2, col2] = jogo[lin2, col2];
+
+                Console.Write("   ");
+                for (int i = 1; i <= 4; i++)
+                {
+                    Console.Write(" {0}  ", i);
+                }
+                Console.WriteLine("\n  -----------------");
+                //Impressão
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write("{0} |", j + 1);
+                    for (int k = 0; k < 4; k++)
+                    {
+                        Console.Write(" {0} |", tela[j, k]);
+                    }
+                    Console.WriteLine("\n  -----------------");
+
+                }
+                Console.WriteLine();
+
+                if (tela[lin1, col1] == tela[lin2, col2])
+                {
+                    acertos++;
+                }
+                else //Caso não tenha acertado o par
+                {
+                    tela[lin1, col1] = 0;
+                    tela[lin2, col2] = 0;
+                }
+                Console.WriteLine("Digite 0 para sair ou outro número para continuar:");
+                int valor = int.Parse(Console.ReadLine());
+
+                if (valor == 0)
+                    break;
+
+            } while (acertos < 8);
+
+
+            TimeSpan timeSpan = DateTime.Now - inicio;
+            Console.WriteLine($"Tempo total de jogo: {timeSpan.ToString(@"hh\:mm\:ss")}");
+
+        }
     }
 }
 
-do
-{
-    //Impressão
-    for (int j = 0; j < 4; j++)
-    {
-        for (int k = 0; k < 4; k++)
-        {
-            Console.Write("{0} ", tela[j, k]);
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-
-    int lin1, col1;
-    Console.WriteLine("Entre com a linha [1, 4]");
-    lin1 = int.Parse(Console.ReadLine());
-    Console.WriteLine("Entre com a coluna [1, 4]");
-    col1 = int.Parse(Console.ReadLine());
-
-    lin1--; //diminuir, pois o usuário não sabe
-    col1--; //que existe linha e coluna zero
-            //Atribuir o valor da matriz jogo na tela
-    tela[lin1, col1] = jogo[lin1, col1];
-
-    //Impressão
-    for (int j = 0; j < 4; j++)
-    {
-        for (int k = 0; k < 4; k++)
-        {
-            Console.Write("{0} ", tela[j, k]);
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-
-    int lin2, col2;
-    Console.WriteLine("Entre com a linha [1, 4]");
-    lin2 = int.Parse(Console.ReadLine());
-    Console.WriteLine("Entre com a coluna [1, 4]");
-    col2 = int.Parse(Console.ReadLine());
-
-    lin2--; //diminuir, pois o usuário não sabe
-    col2--; //que existe linha e coluna zero
-            //Atribuir o valor da matriz jogo na tela
-    tela[lin2, col2] = jogo[lin2, col2];
-
-    //Impressão
-    for (int j = 0; j < 4; j++)
-    {
-        for (int k = 0; k < 4; k++)
-        {
-            Console.Write("{0} ", tela[j, k]);
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-
-    if (tela[lin1, col1] == tela[lin2, col2])
-    {
-        acertos++;
-    }
-    else //Caso não tenha acertado o par
-    {
-        tela[lin1, col1] = 0;
-        tela[lin2, col2] = 0;
-    }
-
-} while (acertos < 8);
 
 
 
